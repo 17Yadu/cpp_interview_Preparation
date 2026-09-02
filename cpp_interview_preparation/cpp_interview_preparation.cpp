0b00f9eb92eb362bda0f1697fd89a6619ea0d0c5
Yadunandan K R <kryadunandan22@gmail.com>
2026-09-02 09:00:14 +0530

    practice

--- cpp_interview_preparation/cpp_interview_preparation.cpp
+++ cpp_interview_preparation/cpp_interview_preparation.cpp
@@ -1,20 +1,54 @@
-// cpp_interview_preparation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//reverse a string in same string 
 
 #include <iostream>
-
+#include<string>
+using namespace std;
+/*
+string s = "hello";
+//reverse length return size_T(unsigned integer type) not int so use static cast to convert it to int
+void reverseString(string s)
+{
+    for (int i = static_cast<int>(s.length())-1;i >= 0;i--)
+    {
+        cout << s[i] <<"\n";
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    reverseString(s);
+    cout << s;// this was giving output as hello even if i reversed . Wrong approach when they ask to reverse a string without creating another one
 }
+*/
+//think of left and right
+//again wrong
+/*
+int main()
+{
+    string s = "hello"; //h is left and o is right
+    int left = s[0];
+    int right = s.length() - 1;
+    while (left >= right)
+    {
+        swap(left, right);
+        left++;
+        right--;
+    }
+    cout << s;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+}
+*/
+//reverse a string to same variable //time complexity is 0(n) because as size increases number of swapping increases for 5 characters we did 2 swaps
+int main()
+{
+    string s = "hello"; //h is left and o is right
+    int left = 0;
+    int right = s.length() - 1; // int right = static_cast<int>(s.length())-1;
+    while (left < right)
+    {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+    cout << s;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
